7e055483a506c35534a7f6916ed93dd27b5cfcf6
신준규 <tlswnsrb1357@gmail.com>
2026-07-13 14:56:02 +0900

    SkipList 제작 완료

--- ZSET/SkipList.h
+++ ZSET/SkipList.h
@@ -1,7 +1,98 @@
 #pragma once
+#include <random>
 
 template<typename Key, typename Value>
 class SkipList {
-public:
 private:
+	static constexpr int MAX_LEVEL = 16;
+	struct Node
+	{
+		Key key;
+		Value value;
+		Node** forward;
+		int level;
+
+		Node(const Key& k, const Value& v, int lvl) {
+			key = k;
+			value = v;
+			level = lvl;
+			forward = new Node*[lvl + 1];
+			for (int i = 0; i <= lvl; ++i) forward[i] = nullptr;
+		}
+		~Node() {
+			delete[] forward;
+		}
+	};
+
+	Node* _head;
+	int _level;
+	int randomLevel() {
+		static std::random_device rd;
+		static std::mt19937 gen(rd());
+		static std::uniform_int_distribution<int> dis(0, 1);
+
+		int lvl = 0;
+
+		while (dis(gen) == 1 && lvl < MAX_LEVEL) lvl++;
+		return lvl;
+	}
+public:
+	SkipList() {
+		_level = 0;
+		_head = new Node(Key{}, Value{}, MAX_LEVEL);
+	}
+	~SkipList() {
+		Node* cur = _head->forward[0];
+		while (cur != nullptr)
+		{
+			Node* next = cur->forward[0];
+			delete cur;
+			cur = next;
+		}
+		delete _head;
+	}
+
+	Node* search(const Key& key) const {
+		Node* cur = _head;
+		for (int i = _level; i >= 0; --i) {
+			while (cur->forward[i] != nullptr && cur->forward[i]->key < key) {
+				cur = cur->forward[i];
+			}
+		}
+		cur = cur->forward[0];
+		if (cur != nullptr && cur->key == key) {
+			return cur;
+		}
+		return nullptr;
+	}
+	void insert(const Key& key, const Value& value) {
+		Node* update[MAX_LEVEL + 1];
+		for (int i = 0; i <= MAX_LEVEL; ++i) update[i] = nullptr;
+
+		Node* cur = _head;
+		for (int i = _level; i >= 0; --i) {
+			while (cur->forward[i] != nullptr && cur->forward[i]->key < key)
+				cur = cur->forward[i];
+			update[i] = cur;
+		}
+
+		cur = cur->forward[0];
+		if (cur != nullptr && cur->key == key) {
+			cur->value = value;
+			return;
+		}
+
+		int lvl = randomLevel();
+		if (lvl > _level) {
+			for (int i = _level + 1; i <= lvl; ++i)
+				update[i] = _head;
+			_level = lvl;
+		}
+
+		Node* n = new Node(key, value, lvl);
+		for (int i = 0; i <= lvl; ++i) {
+			n->forward[i] = update[i]->forward[i];
+			update[i]->forward[i] = n;
+		}
+	}
 };
