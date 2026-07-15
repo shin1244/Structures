26ae6ebe20cc101be05b5a25a34faa4e757d8c93
신준규 <tlswnsrb1357@gmail.com>
2026-07-15 16:27:52 +0900

    타이밍휠 완성

--- TimingWheel/TimingWheel.h
+++ TimingWheel/TimingWheel.h
@@ -1,66 +1,67 @@
 #pragma once
 #include <unordered_map>
+#include "DoubleLinkedList.h"
 
 template<typename Value>
 class TimingWheel
 {
 private:
-	
-	struct Node
+	static constexpr int WHEEL_SIZE = 3600;
+	struct TimerEntry
 	{
 		Value value;
-		int rotation;
-		Node* prev;
-		Node* next;
-
-		Node(const Value& v, const int r) : value(v), rotation(r), prev(nullptr), next(nullptr){}
-		~Node() {}
+		int   rotation;
+		uintptr_t   id;
 	};
-	static constexpr int WHEEL_SIZE = 3600;
-	Node* slots_[WHEEL_SIZE]; 
-	int currentTick_ = 0;
-	std::unordered_map<int, Node*> map_;
+	using List = DoubleLinkedList<TimerEntry>;
+	using Node = typename List::Node;
 
-	void append(int tick, Node* n)
-	{
-		slots_[tick]->prev = n;
-		n->prev = nullptr;
-		n->next = slots_[tick];
-		slots_[tick] = n;
-	}
+	List slots_[WHEEL_SIZE];
+	int currentTick_ = 0;
 public:
-	int add(int delayTicks, const Value& value)
+	uintptr_t add(int delayTicks, const Value& value)
 	{	
 		int insertRot = delayTicks / WHEEL_SIZE;
 		int insertTick = (currentTick_ + delayTicks) % WHEEL_SIZE;
-		Node* n = new Node(value, insertRot);
 
-		append(insertTick, n);
-		return insertTick;
+		TimerEntry entry{ value, insertRot, 0 };
+		Node* n = slots_[insertTick].pushFront(entry);
+		uintptr_t id = reinterpret_cast<uintptr_t>(n);
+		n->value.id = id;
+
+		return id;
 	}
-	void remove(int id);  
+	void remove(uintptr_t id)
+	{
+		if (id == 0) return;
+		Node* targetNode = reinterpret_cast<Node*>(id);
+
+		if (targetNode->prev != nullptr) targetNode->prev->next = targetNode->next;
+		if (targetNode->next != nullptr) targetNode->next->prev = targetNode->prev;
+
+		delete targetNode;
+	}
 	void tick()
 	{
 		currentTick_ = (currentTick_ + 1) % WHEEL_SIZE;
 
 		Node* cur = slots_[currentTick_];
-		Node* prev = nullptr;
 		while (cur != nullptr)
 		{
+			Node* nextNode = cur->next;
 			if (cur->rotation == 0)
 			{
-				Node* nextNode = cur->next;
-				if (prev == nullptr) slots_[currentTick_] = nextNode;
-				else prev->next = nextNode;
+				if (cur->prev != nullptr) cur->prev->next = nextNode;
+				if (nextNode != nullptr) nextNode->prev = cur->prev;
+				if (slots_[currentTick_].head == cur) slots_[currentTick_].head = nextNode;
+
 				delete cur;
-				cur = nextNode;
 			}
 			else
 			{
-				prev = cur;
 				--cur->rotation;
-				cur = cur->next;
 			}
+			cur = nextNode;
 		}
 	}
 };
---
+++ TimingWheel/main.cpp
@@ -0,0 +1,6 @@
+#include "TimingWheel.h"
+
+int main()
+{
+    return 0;
+}
