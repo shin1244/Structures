331a9173201fb0205901f04dfccbf2d11964749a
신준규 <tlswnsrb1357@gmail.com>
2026-07-20 16:05:40 +0900

    Push 완성

--- LockFreeQueue/LockFreeQueue.h
+++ LockFreeQueue/LockFreeQueue.h
@@ -15,4 +15,48 @@ private:
 	std::atomic<size_t> head_{ 0 };
 	std::atomic<size_t> tail_{ 0 };
 public:
+	LockFreeQueue()
+	{
+		for (size_t i = 0; i < N; ++i)
+		{
+			buffer_[i].seq.store(i, std::memory_order_relaxed);
+		}
+	}
+	static constexpr size_t capacity() { return N; }
+
+	bool push(const T& value)
+	{
+		size_t t = tail_.load(std::memory_order_relaxed);
+
+		while (true)
+		{
+			Slot& slot = buffer_[t % N];
+			size_t seq = slot.seq.load(std::memory_order_acquire);
+			intptr_t diff = static_cast<intptr_t>(seq) - static_cast<intptr_t>(t);
+			if (diff == 0)
+			{
+				if (tail_.compare_exchange_weak(t, t + 1, std::memory_order_relaxed))
+				{
+					slot.data = value;
+					slot.seq.store(t + 1, std::memory_order_release);
+					return true;
+				}
+			}
+			else if (diff < 0)
+			{
+				return false;
+			}
+			else
+			{
+				t = tail_.load(std::memory_order_relaxed);
+			}
+		}
+	}
+
+	/*bool pop(T& out)
+	{
+		size_t h = head_.load(std::memory_order_relaxed);
+
+		buffer_[h]
+	}*/
 };
